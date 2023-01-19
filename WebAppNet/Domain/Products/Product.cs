9b484639e0300da2462834d3e7eee5b8e89f6701
dono <PC@DESKTOP-SQFDOVS>
2023-01-19 15:59:12 -0300

    Adicionar arquivos de projeto.

---
+++ WebAppNet/Domain/Products/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAppNet.Domain.Products;
+
+public class Product
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public Category Categoryy { get; set; }
+    public string Description { get; set; }
+    public bool MyProperty { get; set; }
+
+}
